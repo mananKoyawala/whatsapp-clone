59611b07a5259e8a0671d63a41168b4de8e6abf1
mananKoyawala <mananKoyawala5565@gmail.com>
2024-06-26 10:22:49 +0530

    add configuration to clean main.go

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -5,74 +5,35 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/mananKoyawala/whatsapp-clone/config"
 	db "github.com/mananKoyawala/whatsapp-clone/database"
-	"github.com/mananKoyawala/whatsapp-clone/internal/contact"
-	"github.com/mananKoyawala/whatsapp-clone/internal/group"
-	msg "github.com/mananKoyawala/whatsapp-clone/internal/message"
-	"github.com/mananKoyawala/whatsapp-clone/internal/user"
-	"github.com/mananKoyawala/whatsapp-clone/internal/ws"
-	logger "github.com/mananKoyawala/whatsapp-clone/logging"
 	"github.com/mananKoyawala/whatsapp-clone/router"
-	"github.com/mananKoyawala/whatsapp-clone/service/upload"
 )
 
 func main() {
 
+	// load .env
 	if err := godotenv.Load(); err != nil {
 		log.Fatal(err.Error())
 	}
 
+	// get AWS s3 variables
 	region := os.Getenv("AWS_REGION")
 	accessKey := os.Getenv("AWS_ACCESS_KEY")
 	secretKey := os.Getenv("AWS_SECRET_KEY")
 	bucketName := os.Getenv("AWS_BUCKET_NAME")
 
+	// database connection
 	db, err := db.NewDatabase()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	// log.Println("DB connected")
-
-	// initialize loggers
-	userLogger := logger.InitUserLogger()
-	messageLogger := logger.InitMessageLogger()
-	groupLogger := logger.InitGroupLogger()
-	contactLogger := logger.InitContactLogger()
-	wsLogger := logger.InitWSLogger()
-
-	// user
-	userRepository := user.NewUserRepository(db.GetDB(), userLogger)
-	userService := user.NewUserService(userRepository, userLogger)
-	userHandler := user.NewUserHandler(userService, userLogger)
-
-	// group initialization
-	groupRepo := group.NewGroupRepository(db.GetDB(), groupLogger)
-	groupSev := group.NewGroupService(groupRepo, groupLogger)
-	groupHand := group.NewGroupHandler(groupSev, groupLogger)
-
-	// message
-	msgRepo := msg.NewMsgReposritory(db.GetDB(), messageLogger)
-	msgSev := msg.NewMsgService(msgRepo, userRepository, groupRepo, messageLogger)
-	msgHand := msg.NewMsgHandler(msgSev, messageLogger)
-
-	// ws initialization
-	hub := ws.NewHub()
-	wsHandler := ws.NewWsHandler(hub, msgRepo, groupRepo, wsLogger)
-
-	// file upload initialization
-	uploadSev := upload.NewAwsService(region, accessKey, secretKey, bucketName)
-	uploadSev.InitializeAwsSerive(region, accessKey, secretKey)
-	uploadHan := upload.NewAwsHandler(*uploadSev)
-
-	// contact initialization
-	conRepo := contact.NewContactRepo(db.GetDB(), contactLogger)
-	conSev := contact.NewContactServ(conRepo, userRepository, contactLogger)
-	conHand := contact.NewContactHan(conSev, contactLogger)
+	// get Configuration
+	hub := config.Configuration(db.GetDB(), region, bucketName, accessKey, secretKey)
 
 	//run the hub
 	go hub.Run()
 
-	router.SetupRouters(userHandler, wsHandler, msgHand, &uploadHan, &conHand, &groupHand)
 	log.Fatal(router.RunServer("localhost:8080"))
 }
---
+++ server/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"database/sql"
+
+	"github.com/mananKoyawala/whatsapp-clone/internal/contact"
+	"github.com/mananKoyawala/whatsapp-clone/internal/group"
+	msg "github.com/mananKoyawala/whatsapp-clone/internal/message"
+	"github.com/mananKoyawala/whatsapp-clone/internal/user"
+	"github.com/mananKoyawala/whatsapp-clone/internal/ws"
+	logger "github.com/mananKoyawala/whatsapp-clone/logging"
+	"github.com/mananKoyawala/whatsapp-clone/router"
+	"github.com/mananKoyawala/whatsapp-clone/service/upload"
+)
+
+func Configuration(db *sql.DB, region, bucketName, accessKey, secretKey string) *ws.Hub {
+	// initialize loggers
+	userLogger := logger.InitUserLogger()
+	messageLogger := logger.InitMessageLogger()
+	groupLogger := logger.InitGroupLogger()
+	contactLogger := logger.InitContactLogger()
+	wsLogger := logger.InitWSLogger()
+
+	// user
+	userRepository := user.NewUserRepository(db, userLogger)
+	userService := user.NewUserService(userRepository, userLogger)
+	userHandler := user.NewUserHandler(userService, userLogger)
+
+	// group initialization
+	groupRepo := group.NewGroupRepository(db, groupLogger)
+	groupSev := group.NewGroupService(groupRepo, groupLogger)
+	groupHand := group.NewGroupHandler(groupSev, groupLogger)
+
+	// message
+	msgRepo := msg.NewMsgReposritory(db, messageLogger)
+	msgSev := msg.NewMsgService(msgRepo, userRepository, groupRepo, messageLogger)
+	msgHand := msg.NewMsgHandler(msgSev, messageLogger)
+
+	// ws initialization
+	hub := ws.NewHub()
+	wsHandler := ws.NewWsHandler(hub, msgRepo, groupRepo, wsLogger)
+
+	// file upload initialization
+	uploadSev := upload.NewAwsService(region, accessKey, secretKey, bucketName)
+	uploadSev.InitializeAwsSerive(region, accessKey, secretKey)
+	uploadHan := upload.NewAwsHandler(*uploadSev)
+
+	// contact initialization
+	conRepo := contact.NewContactRepo(db, contactLogger)
+	conSev := contact.NewContactServ(conRepo, userRepository, contactLogger)
+	conHand := contact.NewContactHan(conSev, contactLogger)
+
+	router.SetupRouters(userHandler, wsHandler, msgHand, &uploadHan, &conHand, &groupHand)
+
+	return hub
+}
